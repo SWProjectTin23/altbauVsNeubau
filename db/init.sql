aa91c25355f19944bdb2ee78e0fed504ffc842e6
Hiiirsch <hirschmillert.tin23@student.dhbw-heidenheim.de>
2025-07-31 09:31:27 +0200

    Add soft and hard for thresholds

--- db/init.sql
+++ db/init.sql
@@ -8,37 +8,26 @@ CREATE TABLE IF NOT EXISTS sensor_data (
     PRIMARY KEY (device_id, timestamp)
 );
 
-COMMENT ON TABLE sensor_data IS 'Table for storing sensor data from devices';
-COMMENT ON COLUMN sensor_data.device_id IS 'ID of the device that collected the data';
-COMMENT ON COLUMN sensor_data.timestamp IS 'Timestamp of the data collection';
-COMMENT ON COLUMN sensor_data.temperature IS 'Temperature reading from the device';
-COMMENT ON COLUMN sensor_data.humidity IS 'Humidity reading from the device';
-COMMENT ON COLUMN sensor_data.pollen IS 'Pollen count reading from the device';
-COMMENT ON COLUMN sensor_data.particulate_matter IS 'Particulate matter reading from the device';
-
 -- change table to hypertable
 SELECT create_hypertable('sensor_data', 'timestamp', if_not_exists => TRUE);
 
-CREATE TABLE IF NOT EXISTS device_thresholds (
+CREATE TABLE IF NOT EXISTS thresholds (
     id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    temperature_min DECIMAL(5, 2),
-    temperature_max DECIMAL(5, 2),
-    humidity_min DECIMAL(5, 2),
-    humidity_max DECIMAL(5, 2),
-    pollen_min INT,
-    pollen_max INT,
-    particulate_matter_min INT,
-    particulate_matter_max INT,
+    temperature_min_soft DECIMAL(5, 2),
+    temperature_max_soft DECIMAL(5, 2),
+    temperature_min_hard DECIMAL(5, 2),
+    temperature_max_hard DECIMAL(5, 2),
+    humidity_min_soft DECIMAL(5, 2),
+    humidity_max_soft DECIMAL(5, 2),
+    humidity_min_hard DECIMAL(5, 2),
+    humidity_max_hard DECIMAL(5, 2),
+    pollen_min_soft INT,
+    pollen_max_soft INT,
+    pollen_min_hard INT,
+    pollen_max_hard INT,
+    particulate_matter_min_soft INT,
+    particulate_matter_max_soft INT,
+    particulate_matter_min_hard INT,
+    particulate_matter_max_hard INT,
     last_updated TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
 );
-
-COMMENT ON TABLE device_thresholds IS 'Table for storing thresholds for devices';
-COMMENT ON COLUMN device_thresholds.id IS 'Unique identifier for the device thresholds';
-COMMENT ON COLUMN device_thresholds.temperature_min IS 'Minimum temperature threshold for the device';
-COMMENT ON COLUMN device_thresholds.temperature_max IS 'Maximum temperature threshold for the device';
-COMMENT ON COLUMN device_thresholds.humidity_min IS 'Minimum humidity threshold for the device';
-COMMENT ON COLUMN device_thresholds.humidity_max IS 'Maximum humidity threshold for the device';
-COMMENT ON COLUMN device_thresholds.pollen_min IS 'Minimum pollen count threshold for the device';
-COMMENT ON COLUMN device_thresholds.pollen_max IS 'Maximum pollen count threshold for the device';
-COMMENT ON COLUMN device_thresholds.particulate_matter_max IS 'Maximum particulate matter threshold for the device';
-COMMENT ON COLUMN device_thresholds.last_updated IS 'Timestamp of the last update to the thresholds';

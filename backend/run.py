c30767560b3a5d6720a3672023539e3eb97cccb0
Zoehong11 <zoehongzy@gmail.com>
2025-08-04 10:41:58 +0200

    feat(backend/mqtt): mqtt ingestor
    mqtt broker-> db

--- backend/run.py
+++ backend/run.py
@@ -1,8 +1,8 @@
-from app import create_app
+from api import create_app
 
 app = create_app()
 
 # debugger should not be enabled in production
 # mac, use port=5001
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    app.run(debug=True, host="0.0.0.0", port=5001)

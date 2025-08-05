d00c8cbec923ce490b763217d039d2401b73d708
Hiiirsch <hirschmillert.tin23@student.dhbw-heidenheim.de>
2025-08-05 09:49:11 +0200

    Changed int to float for temperature and humiditiy

--- frontend/src/Dashboard.jsx
+++ frontend/src/Dashboard.jsx
@@ -170,14 +170,14 @@ export default function Dashboard() {
 
         const mapped = {
           Altbau: {
-            Temperatur: altbauJson.data.temperature,
-            Luftfeuchtigkeit: altbauJson.data.humidity,
+            Temperatur: parseFloat(altbauJson.data.temperature),
+            Luftfeuchtigkeit: parseFloat(altbauJson.data.humidity),
             Pollen: altbauJson.data.pollen,
             Feinpartikel: altbauJson.data.particulate_matter,
           },
           Neubau: {
-            Temperatur: neubauJson.data.temperature,
-            Luftfeuchtigkeit: neubauJson.data.humidity,
+            Temperatur: parseFloat(neubauJson.data.temperature),
+            Luftfeuchtigkeit: parseFloat(neubauJson.data.humidity),
             Pollen: neubauJson.data.pollen,
             Feinpartikel: neubauJson.data.particulate_matter,
           },

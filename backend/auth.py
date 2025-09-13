8218e46a130af7151527e12d091d265faf3eedbd
marc55s <marc.schoenig@gmail.com>
2025-09-13 02:06:18 +0200

    fix: docker depends_on, backend response type

--- backend/auth.py
+++ backend/auth.py
@@ -4,15 +4,15 @@ import jwt
 from flask import request, jsonify
 from functools import wraps
 
-JWKS_URL = f"{os.getenv('JWKS_URL')}"
-CLIENT_ID = f"{os.getenv('CLIENT_ID')}"
+JWKS_URL = os.getenv('JWKS_URL')
+CLIENT_ID = os.getenv('CLIENT_ID')
 
-print(JWKS_URL)
-jwks = requests.get(JWKS_URL).json()
+print(CLIENT_ID, JWKS_URL)
 
 def get_public_key(token):
     header = jwt.get_unverified_header(token)
     kid = header['kid']
+    jwks = requests.get(JWKS_URL).json()
     for key in jwks['keys']:
         if key['kid'] == kid:
             return jwt.algorithms.RSAAlgorithm.from_jwk(key)
@@ -27,12 +27,19 @@ def token_required(f):
             if auth_header.startswith("Bearer "):
                 token = auth_header.split(" ")[1]
         if not token:
-            return jsonify({"message": "Token is missing"}), 401
+            return {"message": "Token is missing"}, 401
+        
         try:
             public_key = get_public_key(token)
-            decoded = jwt.decode(token, public_key, algorithms=['RS256'], audience=CLIENT_ID)
+            decoded = jwt.decode(token, public_key, algorithms=['RS256'], options={"verify_aud": False})
+            
+            if decoded.get('azp') != CLIENT_ID:
+                raise Exception(f"Authorized party mismatch: expected {CLIENT_ID}, got {decoded.get('azp')}")
+            
             request.user = decoded
+            
         except Exception as e:
-            return jsonify({"message": "Token is invalid", "error": str(e)}), 401
+            return {"message": "Token is invalid", "error": str(e)}, 401
+        
         return f(*args, **kwargs)
     return decorated
